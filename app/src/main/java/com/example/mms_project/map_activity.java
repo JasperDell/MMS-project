9552590b5781f5a2463664f1dd9397bff801eed2
Jasper Dellaert <jasper.del@hotmail.com>
2021-11-30 19:01:54 +0100

    Checks if there is an iamage saved else use a default one

--- app/src/main/java/com/example/mms_project/map_activity.java
+++ app/src/main/java/com/example/mms_project/map_activity.java
@@ -10,7 +10,14 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
 import android.location.Location;
 import android.os.Bundle;
@@ -35,8 +42,17 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -49,7 +65,7 @@ import java.util.concurrent.TimeUnit;
 
 // Implement OnMapReadyCallback.
 public class map_activity extends AppCompatActivity implements OnMapReadyCallback {
-
+    private FirebaseAuth mAuth;
     //Session info
     MapView mapView;
     GoogleMap googleMap;
@@ -73,6 +89,7 @@ public class map_activity extends AppCompatActivity implements OnMapReadyCallbac
         if(curr != null){
             person_inf = curr;
             person_inf.setLastLoc(52.08545962113923, 4.33562457561493);
+            person_inf.uID = mAuth.getCurrentUser().getUid();
         }
         else {
             //set user
@@ -97,20 +114,75 @@ public class map_activity extends AppCompatActivity implements OnMapReadyCallbac
         local_inf.add(temp_inf);
     }
 
+    private Bitmap getRoundedCroppedBitmap(Bitmap bitmap) {
+        bitmap = Bitmap.createScaledBitmap(bitmap,BMAP_DIM,BMAP_DIM, false);
+        int widthLight = bitmap.getWidth();
+        int heightLight = bitmap.getHeight();
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+        Paint paintColor = new Paint();
+        paintColor.setFlags(Paint.ANTI_ALIAS_FLAG);
+        RectF rectF = new RectF(new Rect(0, 0, widthLight, heightLight));
+        canvas.drawRoundRect(rectF, widthLight / 2, heightLight / 2, paintColor);
+        Paint paintImage = new Paint();
+        paintImage.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP));
+        canvas.drawBitmap(bitmap, 0, 0, paintImage);
+        return output;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         System.out.println("Starting up map");
-
+        mAuth = FirebaseAuth.getInstance();
         //UserMap reg = (UserMap) getIntent().getSerializableExtra("usr");
-        Boolean reg = (boolean) getIntent().getExtras().get("reg");
-        if(reg != null && reg){
+        //Boolean reg = (boolean) getIntent().getExtras().get("reg");
+        //if(reg != null && reg){
+        if (mAuth.getCurrentUser() != null){
             UserMap register = new UserMap();
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            DatabaseReference mDatabase = FirebaseDatabase.getInstance("https://mms-project-a6f37-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
+            mDatabase.child("users").child(currentUser.getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    if (!task.isSuccessful()) {
+                        register.firstName = (String) getIntent().getExtras().get("fname");
+                        register.lastName = (String) getIntent().getExtras().get("lname");
+                        register.bio = (String) getIntent().getExtras().get("bio");
+                        register.age = (int) getIntent().getExtras().get("age");
+                    }
+                    else {
+                        HashMap<String, Object> result = (HashMap<String, Object>) task.getResult().getValue();
+                        register.firstName = (String) result.get("first_name");
+                        register.lastName = (String) result.get("last_name");
+                        register.bio = (String) result.get("bio");
+                        register.age = ((Long) result.get("age")).intValue();
+                    }
+                }
+            });
 
-            register.firstName = (String) getIntent().getExtras().get("fname");
-            register.lastName = (String) getIntent().getExtras().get("lname");
-            register.bio = (String) getIntent().getExtras().get("bio");
-            register.age = (int) getIntent().getExtras().get("age");
-            register.icon = (Bitmap) getIntent().getExtras().get("img");
+            FirebaseUser user = mAuth.getCurrentUser();
+            FirebaseStorage storage = FirebaseStorage.getInstance("gs://mms-project-a6f37.appspot.com/");
+            StorageReference storageRef = storage.getReference();
+            StorageReference imagesRef = storageRef.child("images");
+            StorageReference fileRef = imagesRef.child(user.getUid());
+            final long ONE_MEGABYTE = 1024 * 1024;
+            fileRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                @Override
+                public void onSuccess(byte[] bytes) {
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                    //Hier getRoundedCroppedBitmap doen werkt niet om een reden?????
+                    //register.icon = getRoundedCroppedBitmap(bitmap);
+                    register.icon = bitmap;
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    register.icon = ((BitmapDrawable) getDrawable(R.drawable.default_man)).getBitmap();
+                }
+            });
+
+
+
             set_defaults(register);
         }
         else {
@@ -264,14 +336,48 @@ public class map_activity extends AppCompatActivity implements OnMapReadyCallbac
         if (googleMap == null)
             return;
         LatLng latLng = new LatLng(user.lastLoc.getLatitude(), user.lastLoc.getLongitude());
-        Bitmap bmap = user.icon;
-        BitmapDescriptor res = BitmapDescriptorFactory.fromBitmap(Bitmap.createScaledBitmap(bmap,BMAP_DIM,BMAP_DIM, false));
-        Marker m = googleMap.addMarker(new MarkerOptions()
-                .position(latLng)
-                .title(user.firstName)
-                .icon(res)
-        );
-        m.setTag(user);
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (!(user.uID.equals(""))) {
+            FirebaseStorage storage = FirebaseStorage.getInstance("gs://mms-project-a6f37.appspot.com/");
+            StorageReference storageRef = storage.getReference();
+            StorageReference imagesRef = storageRef.child("images");
+            StorageReference fileRef = imagesRef.child(user.uID);
+            final long ONE_MEGABYTE = 1024 * 1024;
+            fileRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                @Override
+                public void onSuccess(byte[] bytes) {
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                    BitmapDescriptor res = BitmapDescriptorFactory.fromBitmap(Bitmap.createScaledBitmap(bitmap, BMAP_DIM, BMAP_DIM, false));
+                    Marker m = googleMap.addMarker(new MarkerOptions()
+                            .position(latLng)
+                            .title(user.firstName)
+                            .icon(res)
+                    );
+                    m.setTag(user);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    Bitmap bmap = ((BitmapDrawable) getDrawable(R.drawable.default_man)).getBitmap();
+                    BitmapDescriptor res = BitmapDescriptorFactory.fromBitmap(Bitmap.createScaledBitmap(bmap, BMAP_DIM, BMAP_DIM, false));
+                    Marker m = googleMap.addMarker(new MarkerOptions()
+                            .position(latLng)
+                            .title(user.firstName)
+                            .icon(res)
+                    );
+                    m.setTag(user);
+                }
+            });
+        } else{
+            Bitmap bmap = user.icon;
+            BitmapDescriptor res = BitmapDescriptorFactory.fromBitmap(Bitmap.createScaledBitmap(bmap,BMAP_DIM,BMAP_DIM, false));
+            Marker m = googleMap.addMarker(new MarkerOptions()
+                    .position(latLng)
+                    .title(user.firstName)
+                    .icon(res)
+            );
+            m.setTag(user);
+        }
 
         if(focus){
             //googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
