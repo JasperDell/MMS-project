4f357e850f584c54b1065853207ac057ca16a431
Jasper Dellaert <jasper.del@hotmail.com>
2021-11-30 15:33:00 +0100

    registreren werkt

---
+++ app/src/main/java/com/example/mms_project/DataBaseUser.java
@@ -0,0 +1,17 @@
+package com.example.mms_project;
+
+public class DataBaseUser {
+
+    public String first_name;
+    public String last_name;
+    public String email;
+
+    public DataBaseUser() {
+    }
+
+    public DataBaseUser(String first_name, String last_name, String email) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+    }
+}
