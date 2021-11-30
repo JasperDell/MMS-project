c34abd33e9b512ab221f737c16a952928249f844
Jasper Dellaert <jasper.del@hotmail.com>
2021-11-30 13:18:21 +0100

    changes to login

--- app/src/main/java/com/example/mms_project/registerForm.java
+++ app/src/main/java/com/example/mms_project/registerForm.java
@@ -2,6 +2,7 @@ package com.example.mms_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -12,13 +13,21 @@ import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.content.Context;
 import com.example.mms_project.R;
 import com.example.mms_project.UserMap;
 import com.example.mms_project.map_activity;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -28,6 +37,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class registerForm extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
     String email = null;
     UserMap register = new UserMap();
@@ -42,6 +53,8 @@ public class registerForm extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         setContentView(R.layout.activity_register_form);
 
         Bundle b = getIntent().getExtras();
@@ -50,6 +63,21 @@ public class registerForm extends AppCompatActivity {
 
         context = getApplicationContext();
 
+    }
+
+    private void showDialog(String title, String msg){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialogue);
+        ((TextView) dialog.findViewById(R.id.txtTitle)).setText(title);
+        ((TextView) dialog.findViewById(R.id.txtDesc)).setText(msg);
+        Button btnClose = dialog.findViewById(R.id.btn_ok);
+        btnClose.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 
     public void addIcon(View view){
@@ -120,8 +148,21 @@ public class registerForm extends AppCompatActivity {
 
     }
 
-    public void onClickRegister(){
+    public void onClickRegister(View view) {
         if(!checkRegistrationValid()){
+            this.showDialog("Error 11", "register not valid");
+            return;
+        }
+        String password = "test123";
+        if (email == null){
+            this.showDialog("Error 11", "email=null");
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(email, password);
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null){
+            this.showDialog("Error 11", "Can't login firebase");
             return;
         }
 
@@ -132,6 +173,9 @@ public class registerForm extends AppCompatActivity {
 
         //Communicate with server and wait for response
         sendUserToServer();
+
+        //mDatabase.child("users").child(currentUser.getUid()).setValue(register);
+        //UserMap test = (UserMap) mDatabase.child("users").child(currentUser.getUid()).get().getResult().getValue();
 
         //Go to the mapview
         Intent intent = new Intent(this, map_activity.class);
